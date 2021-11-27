bff703aa0e1560965144be5622c2ea82b24dd585
Pavel E. Petrov <admin@kamenka.su>
2021-11-27 15:36:14 +0300

    comments
    Signed-off-by: Pavel E. Petrov <admin@kamenka.su>

--- opencv_cpu_only.py
+++ opencv_cpu_only.py
@@ -4,6 +4,9 @@ import cv2
 def bench(image_file, gdim=3, rep=100):
     # load an image
     img = cv2.imread(image_file, cv2.IMREAD_COLOR)
+    # repeat task rep times
     for iteration in range(rep):
+        # blur image with gdim x gdim conv mtx
         img = cv2.GaussianBlur(img,(gdim,gdim),0)
+    # show result image
     cv2.imshow("CPU", img)
--- opencv_cuda.py
+++ opencv_cuda.py
@@ -3,10 +3,15 @@ import cv2
 def bench(image_file, gdim=1, rep=100):
     # load an image
     img = cv2.imread(image_file, cv2.IMREAD_COLOR)
+    # upload into GPU memory
     src = cv2.cuda_GpuMat()
     src.upload(img)
+    # prepare blur kernel with gdim x gdim conv mtx
     blur = cv2.cuda.createGaussianFilter(cv2.CV_8UC3,cv2.CV_8UC3,(gdim,gdim),0)
+    # repeat task rep times
     for iteration in range(rep):
         blur.apply(src,src)
+    # download result image into host memory
     result = src.download()
+    # show result image
     cv2.imshow("CUDA", result)
